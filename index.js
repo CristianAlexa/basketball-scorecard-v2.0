2e8183ff0190a999c0c2b657b12de88e5f8b7e73
CristianAlexa <94307268+CristianAlexa@users.noreply.github.com>
2022-09-06 22:15:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let homeScore = 0
+let guestScore = 0
+
+// let homePoints 
+// let guestPoints 
+// let target
+
+function addPoints(target, val) {
+    console.log(target)
+    if (target === "homePoints") {
+    homeScore += val
+    document.getElementById("home-points").textContent = homeScore
+    } else {
+    guestScore += val
+    document.getElementById("guest-points").textContent = guestScore
+    }
+}
+
+function newGame() {
+    homeScore = 0 
+    guestScore = 0
+    document.getElementById("home-points").textContent = homeScore
+    document.getElementById("guest-points").textContent = guestScore
+}
+
